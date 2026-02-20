8ba435932352efe41dd13134610a13e6c1882793
chinuu969 <chinmayeedeo969@gmail.com>
2026-02-20 12:26:32 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,23 +1,9 @@
-"""
-URL configuration for myproject project.
+from django.urls import path
+from.import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('onlinecourse.urls')),
-]
+urlpatterns=[path('', views.index,name='index'),]
+path('course/<int:course_id>/submit/', views.submit_exam, name='submit_exam'),
+path('course/<int:course_id>/submission/<int:submission_id>/',
+     views.show_exam_result,
+     name='exam_result'),
+path('course/<int:course_id>/exam/', views.take_exam, name='take_exam'),
